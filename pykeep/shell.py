b658828531101503960227cf5eb6683d05f60174
Paul Varet <paul.varet@epita.fr>
2019-05-26 01:29:34 +0200

    [SHELL] rework commands storage

--- pykeep/shell.py
+++ pykeep/shell.py
@@ -5,35 +5,33 @@ from config import Config
 
 class Shell():
 
-    options = ['ls', 'exit']
-
     def __init__(self, prompt: str, conf: Config):
         self.prompt = prompt
         self.conf = conf
+        self.commands = {
+                'ls': self.ls,
+        }
         readline.parse_and_bind('tab: complete')
 
-    def ls(self) -> None:
+    def ls(self, args: list) -> None:
         subprocess.run(['ls', '--color=auto', self.conf.path])
 
-    @staticmethod
-    def complete(text, state):
-        results = [cmd for cmd in Shell.options if cmd.startswith(text)]
+    #@staticmethod
+    def complete(self, text, state):
+        results = [cmd for cmd in self.commands.keys()  if cmd.startswith(text)]
         return results[state]
 
     def loop(self) -> None:
-        commands = {
-                'ls': self.ls,
-        }
-        readline.set_completer(Shell.complete)
+        readline.set_completer(self.complete)
         while 1:
             try:
-                line = input(self.prompt)
-                if line == 'exit':
+                line = input(self.prompt).split(' ')
+                if line[0] == 'exit':
                     break
                 try:
-                    commands[line]()
+                    self.commands[line[0]](line[1:])
                 except KeyError:
-                    print(f'pykeep: command not found: {line}')
+                    print(f'pykeep: command not found: {line[0]}')
             except EOFError:
                 print()
-                return
+                break
